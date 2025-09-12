cb1e6aecabc626a3fad3103bbfe82aafea2a18d7
Ayaan Rizvi <2002622@apps.nsd.org>
2025-09-12 17:00:39 +0000

    I GOT IT

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -16,7 +16,7 @@ class Main {
     public static void main(String[] args) {
       System.out.println("-----------------");
       System.out.println("!!!! PROGRAM START !!!!");
-      System.out.println();
+      System.out.println("lalalalala");
       System.out.println("hello");
       System.out.println("!!!! PROGRAM END !!!!");
       System.out.println("-----------------");
