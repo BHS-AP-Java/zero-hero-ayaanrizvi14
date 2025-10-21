0c126abad4139bb671cd0a3d969ab8f17d33b2f9
Ayaan Rizvi <2002622@apps.nsd.org>
2025-10-21 16:27:17 +0000

    I can make an app file

---
+++ src/main/java/edu/bhscs/App.java
@@ -0,0 +1,16 @@
+package edu.bhscs;
+
+public class App{
+public static void main(String[] args){
+
+  data_10_21();
+
+  System.out.println("I am the app");
+
+}
+
+public static void data_10_21(){
+  System.out.println("Stuff today");
+}
+
+}
