23e0ef08f90413ac45301a2362f72ad5f5fc347b
Ayaan Rizvi <2002622@apps.nsd.org>
2025-10-29 01:29:08 +0000

    added a description, inout, output, edgecase

--- src/main/java/edu/bhscs/Table.java
+++ src/main/java/edu/bhscs/Table.java
@@ -1,3 +1,19 @@
+// Ayaan Rizvi
+// P2
+// Zero-Hero
+// 10/28/2025
+
+/*
+ * DESCRIPTION: The table class represents a table with legs and a top, allowing customization of its appearance.There will also be a cake centered on the table
+ * Input: Number of legs and width of the table
+ * Output: A visual representation of the table with a cake in the terminal
+ * Edge Case: I dont think there are any but maybe having to deal with the fencepost problem
+ */
+
+
+
+
+
 package edu.bhscs;
 
 public class Table {
@@ -25,7 +41,7 @@ public class Table {
   // here is where I will create the top of the table
 
   // Here is where I will create the legs of the table
-  
+
   // need loops for both of these
 
 
